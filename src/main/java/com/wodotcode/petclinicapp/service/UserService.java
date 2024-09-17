9281e0dd4b0c409e7751e92b14e60802f0e7d47c
Wendell Centeno <dellortiz06@gmail.com>
2024-09-17 22:06:04 +0800

    Create UserService

---
+++ src/main/java/com/wodotcode/petclinicapp/service/UserService.java
@@ -0,0 +1,19 @@
+package com.wodotcode.petclinicapp.service;
+
+
+import com.wodotcode.petclinicapp.model.User;
+import com.wodotcode.petclinicapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void add(User user) {
+        userRepository.save(user);
+
+    }
+}
