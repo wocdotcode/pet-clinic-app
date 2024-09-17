5ceeef9cf2e66813d000dc99c72e13b5f72cdf94
Wendell Centeno <dellortiz06@gmail.com>
2024-09-17 22:08:31 +0800

    Added PetClinicApplication.java

---
+++ src/main/java/com/wodotcode/petclinicapp/PetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.wodotcode.petclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicAppApplication.class, args);
+	}
+
+}
